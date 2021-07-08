1960a84fc729f8661a929c24b6ed061d46fba6d6
GermanRomeroCruz <germancarab@gmail.com>
2021-07-08 22:33:51 +0200

    página completa guardando en localstorage

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interface/giffs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+  
+  private  _apiKey: string = "cHDyp6lq9KR00XvKH3ZzyzAvtYlRk6zC";
+  
+  private _servicioUrl = 'https://api.giphy.com/v1/gifs';
+
+  private _historial: string[] = [];
+
+  public resultados: Gif[] = [];
+
+  get historial(){
+    
+    return [...this._historial];
+  }
+  
+
+  constructor( private http: HttpClient) {
+    
+    this._historial = JSON.parse( localStorage.getItem( 'historial' )! ) || [];
+    this.resultados = JSON.parse ( localStorage.getItem( 'resultados' )! ) || [];
+    // if( localStorage.getItem('historial') ) {
+
+    //   this._historial = JSON.parse( localStorage.getItem('historial')! );
+
+    // }
+
+  }
+
+  buscarGifs( query: string) {
+
+    query = query.trim().toLocaleLowerCase();
+
+    if( !this._historial.includes( query ) ) {
+      this._historial.unshift( query );
+      this._historial = this._historial.splice(0,10);
+      localStorage.setItem('historial', JSON.stringify( this._historial ) );
+      
+    }
+    
+    const params = new HttpParams().set('api_key',this._apiKey).set('limit','10').set('q',query);
+
+    
+    this.http.get<SearchGifsResponse>(`${this._servicioUrl}/search`, { params } )
+    .subscribe( (resp) => {
+      this.resultados = resp.data;
+      localStorage.setItem( 'resultados', JSON.stringify( this.resultados ) );
+    });
+  }
+}
